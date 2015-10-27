0b6744be6de4264842365a67970bf6815876cd9f
zethra <jediben97@gmail.com>
2015-10-27 12:08:27 -0400

    Reorganized editor code

--- web/static/js/editor.js
+++ web/static/js/editor.js
@@ -2,80 +2,12 @@ var editor;
 
 requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
 
+    //Load the editor
     editor = ace.edit('editor');
     editor.setTheme('ace/theme/monokai');
     editor.getSession().setMode('ace/mode/' + lang);
 
-    var fileName = document.title;
-
-    function save() {
-        var data = new FormData();
-        data.append('file', editor.getValue());
-        var boundary = "---------------------------7da24f2e50046";
-        var body = '--' + boundary + '\r\n'
-            + 'Content-Disposition: form-data; name="file";'
-            + 'filename="temp.txt"\r\n'
-            + 'Content-type: plain/text\r\n\r\n'
-            + editor.getValue() + '\r\n'
-            + '--'+ boundary + '--';
-        $.ajax({
-            type: 'POST',
-            url: window.location.pathname,
-            contentType: "multipart/form-data; boundary="+boundary,
-            data: body
-        }).done(function(response){
-            console.log("Save successful: " + response);
-            editor.session.getUndoManager().markClean();
-            $('#save').prop("disabled",true);
-            document.title = fileName;
-            window.onbeforeunload = null;
-        }).fail(function(response){
-            console.log("Save failed: " + response);
-        });
-    }
-
-    $('#save').on('click', function(){
-        save();
-    });
-
-    editor.commands.addCommand({
-        name: 'save',
-        bindKey: {win: 'Ctrl-S', mac: 'Command-S'},
-        exec: function(editor) {
-            if (!editor.session.getUndoManager().isClean()) {
-                save();
-            }
-        },
-        readOnly: true
-    });
-
-    mousetrap.bind(['ctrl+s', 'command+s'], function(e){
-        if (e.preventDefault) {
-            e.preventDefault();
-            console.log("Prevented")
-        } else {
-            // internet explorer
-            e.returnValue = false;
-        }
-        if (!editor.session.getUndoManager().isClean()) {
-            save();
-        }
-        return false;
-    });
-
-    editor.on('input', function() {
-        if (editor.session.getUndoManager().isClean()) {
-            $('#save').prop("disabled",true);
-            document.title = fileName;
-            window.onbeforeunload = null;
-        }
-        else {
-            $('#save').prop("disabled",false);
-            document.title = "* " + fileName;
-            window.onbeforeunload = function(){return "You have unsaved changes are you sure you want to exit?"};
-        }
-    });
-
+    //UI setup
     if(fileType != '') {
         $('#' + fileType).parent().addClass('active');
     }
@@ -107,5 +39,78 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
         $('#fileViewExpand').addClass('hidden')
             .parent().addClass('col-md-2').removeClass('nav-sidebar-collapsed');
         $('#fileViewShrink').parent().parent().removeClass('hidden');
+    });
+
+    //Setup save function
+    var fileName = document.title;
+
+    function save() {
+        var data = new FormData();
+        data.append('file', editor.getValue());
+        var boundary = "---------------------------7da24f2e50046";
+        var body = '--' + boundary + '\r\n'
+            + 'Content-Disposition: form-data; name="file";'
+            + 'filename="temp.txt"\r\n'
+            + 'Content-type: plain/text\r\n\r\n'
+            + editor.getValue() + '\r\n'
+            + '--'+ boundary + '--';
+        $.ajax({
+            type: 'POST',
+            url: window.location.pathname,
+            contentType: "multipart/form-data; boundary="+boundary,
+            data: body
+        }).done(function(response){
+            console.log("Save successful: " + response);
+            editor.session.getUndoManager().markClean();
+            $('#save').prop("disabled",true);
+            document.title = fileName;
+            window.onbeforeunload = null;
+        }).fail(function(response){
+            console.log("Save failed: " + response);
+        });
+    }
+
+    //Setup save UI
+    $('#save').on('click', function(){
+        save();
+    });
+
+    editor.on('input', function() {
+        if (editor.session.getUndoManager().isClean()) {
+            $('#save').prop("disabled",true);
+            document.title = fileName;
+            window.onbeforeunload = null;
+        }
+        else {
+            $('#save').prop("disabled",false);
+            document.title = "* " + fileName;
+            window.onbeforeunload = function(){return "You have unsaved changes are you sure you want to exit?"};
+        }
+    });
+
+    //Bin save keys
+    editor.commands.addCommand({
+        name: 'save',
+        bindKey: {win: 'Ctrl-S', mac: 'Command-S'},
+        exec: function(editor) {
+            if (!editor.session.getUndoManager().isClean()) {
+                save();
+            }
+        },
+        readOnly: true
+    });
+
+    mousetrap.bind(['ctrl+s', 'command+s'], function(e){
+        if (e.preventDefault) {
+            e.preventDefault();
+            console.log("Prevented")
+        } else {
+            // internet explorer
+            e.returnValue = false;
+        }
+        if (!editor.session.getUndoManager().isClean()) {
+            save();
+        }
+        return false;
     });
 });
