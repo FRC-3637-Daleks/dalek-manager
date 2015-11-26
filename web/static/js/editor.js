4a928cc7f821c7e7944852645a22a07c7bce1a85
zethra <jediben97@gmail.com>
2015-11-25 21:11:07 -0500

    Commented editor file

--- web/static/js/editor.js
+++ web/static/js/editor.js
@@ -1,10 +1,13 @@
 var editor;
+//get libraries
 requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
 
     var nameChange = false;
     var originalName;
 
+    //Save the file
     function save() {
+        //Build file upload request with dummy file containing file content
         var data = new FormData();
         data.append('file', editor.getValue());
         var boundary = "---------------------------7da24f2e50046";
@@ -12,36 +15,39 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
             + 'Content-Disposition: form-data; name="file";'
             + 'filename="temp.txt"\r\n'
             + 'Content-type: plain/text\r\n\r\n'
-            + editor.getValue() + '\r\n'
-            + '--'+ boundary + '--';
+            + editor.getValue() + '\r\n';
+        //Send file to server
         $.ajax({
             type: 'POST',
             url: '/file/' + fileType + '/' + fileName,
             contentType: "multipart/form-data; boundary="+boundary,
             data: body
         }).done(function(response){
+            //Update editor UI
             console.log("Save successful: " + response);
             editor.session.getUndoManager().markClean();
+            //Update editor URL and lang if file name is changed
             if(nameChange) {
                 window.history.pushState(null, null, '/editor/' + fileType + '/' + fileName);
+                nameChange = false;
+                var temp = fileName.split('.');
+                if(temp.length == 2) {
+                    lang = temp[1];
+                } else {
+                    lang = 'text';
+                }
+                if(lang == 'txt') {
+                    lang = 'text';
+                }
+                editor.getSession().setMode('ace/mode/' + lang);
             }
-            nameChange = false;
-            var temp = fileName.split('.');
-            if(temp.length == 2) {
-                lang = temp[1];
-            } else {
-                lang = 'text';
-            }
-            if(lang == 'txt') {
-                lang = 'text';
-            }
-            editor.getSession().setMode('ace/mode/' + lang);
             updateEditorState()
         }).fail(function(response){
             console.log("Save failed: " + response);
         });
     }
 
+    //Change editor based on if file has been edited since last save
     function updateEditorState() {
         if (editor.getSession().getUndoManager().isClean() && !nameChange) {
             //console.log('yes', editor.session.getUndoManager().isClean(), !nameChange);
@@ -73,7 +79,6 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
         },
         readOnly: true
     });
-    editor.getSession().on('change', function(){updateEditorState()});
 
     mousetrap.bind(['ctrl+s', 'command+s'], function(e){
         if (e.preventDefault) {
@@ -88,22 +93,23 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
         return false;
     });
 
+    //Update editor state when file is chnaged
+    editor.getSession().on('change', function(){updateEditorState()});
+
     $(document).ready(function(){
         //UI setup
+        var fileNameBox = $('#fileName');
         if(fileType != '') {
             $('#' + fileType).parent().addClass('active');
             $('#fileNameContainer').find('label').html(fileType + '/')
         }
         if(fileName != '') {
-            $('#fileName').attr('value', fileName);
+            fileNameBox.attr('value', fileName);
         }
-
-        //Setup save UI
         $('#save').on('click', function(){
             save();
         });
-
-        $('#fileName').on('input', function(e){
+        fileNameBox.on('input', function(e){
             if($(e.target).val() != originalName) {
                 nameChange = true;
                 fileName = $(e.target).val();
@@ -113,7 +119,6 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
             }
             updateEditorState();
         });
-
         updateEditorState();
     });
 });
