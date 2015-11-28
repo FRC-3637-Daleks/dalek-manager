2d84ca88446c93c5613c97ac193720f60493cd93
zethra <jediben97@gmail.com>
2015-11-28 18:22:17 -0500

    Fixed editor bug

--- web/static/js/editor.js
+++ web/static/js/editor.js
@@ -1,13 +1,53 @@
 var editor;
-//get libraries
+
 requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
 
     var nameChange = false;
     var originalName;
 
-    //Save the file
+    //UI setup
+    if(fileType != '') {
+        $('#' + fileType).parent().addClass('active');
+    }
+
+    $('#folderViewShrink').find('> span').on('click', function(){
+        $('#folderViewShrink').parent().parent().addClass('hidden');
+        $('#folderViewExpand').removeClass('hidden')
+            .parent().removeClass('col-md-2').addClass('nav-sidebar-collapsed');
+        $('#editor').parent().removeClass('col-md-10 col-md-offset-2').addClass('editor-sidebar-collapsed');
+        $('.nav-bottom').removeClass('col-md-offset-2').addClass('editor-sidebar-collapsed');
+
+    });
+
+    $('#folderViewExpand').on('click', function(){
+        $('#editor').parent().removeClass('editor-sidebar-collapsed').addClass('col-md-10 col-md-offset-2');
+        $('#folderViewExpand').addClass('hidden')
+            .parent().addClass('col-md-2').removeClass('nav-sidebar-collapsed');
+        $('#folderViewShrink').parent().parent().removeClass('hidden');
+        $('.nav-bottom').addClass('col-md-offset-2').removeClass('editor-sidebar-collapsed');
+    });
+
+    $('#fileViewShrink').find('> span').on('click', function(){
+        $('#fileViewShrink').parent().parent().addClass('hidden');
+        $('#fileViewExpand').removeClass('hidden')
+            .parent().removeClass('col-md-2').addClass('nav-sidebar-collapsed');
+    });
+
+    $('#fileViewExpand').on('click', function(){
+        $('#fileViewExpand').addClass('hidden')
+            .parent().addClass('col-md-2').removeClass('nav-sidebar-collapsed');
+        $('#fileViewShrink').parent().parent().removeClass('hidden');
+    });
+
+    //Load the editor
+    editor = ace.edit('editor');
+    editor.setTheme('ace/theme/monokai');
+    editor.getSession().setMode('ace/mode/' + lang);
+
+    //Setup save function
+    var fileName = document.title;
+
     function save() {
-        //Build file upload request with dummy file containing file content
         var data = new FormData();
         data.append('file', editor.getValue());
         var boundary = "---------------------------7da24f2e50046";
@@ -15,60 +55,47 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
             + 'Content-Disposition: form-data; name="file";'
             + 'filename="temp.txt"\r\n'
             + 'Content-type: plain/text\r\n\r\n'
-            + editor.getValue() + '\r\n';
-        //Send file to server
+            + editor.getValue() + '\r\n'
+            + '--'+ boundary + '--';
         $.ajax({
             type: 'POST',
             url: '/file/' + fileType + '/' + fileName,
             contentType: "multipart/form-data; boundary="+boundary,
             data: body
         }).done(function(response){
-            //Update editor UI
             console.log("Save successful: " + response);
             editor.session.getUndoManager().markClean();
-            //Update editor URL and lang if file name is changed
-            if(nameChange) {
-                window.history.pushState(null, null, '/editor/' + fileType + '/' + fileName);
-                nameChange = false;
-                var temp = fileName.split('.');
-                if(temp.length == 2) {
-                    lang = temp[1];
-                } else {
-                    lang = 'text';
-                }
-                if(lang == 'txt') {
-                    lang = 'text';
-                }
-                editor.getSession().setMode('ace/mode/' + lang);
-            }
-            updateEditorState()
+            $('#save').prop("disabled",true);
+            document.title = fileName;
+            window.onbeforeunload = null;
         }).fail(function(response){
             console.log("Save failed: " + response);
         });
     }
 
-    //Change editor based on if file has been edited since last save
+    //Setup save UI
+    $('#save').on('click', function(){
+        save();
+    });
+
     function updateEditorState() {
-        if (editor.getSession().getUndoManager().isClean() && !nameChange) {
-            //console.log('yes', editor.session.getUndoManager().isClean(), !nameChange);
-            //$('#save').addClass("disabled");
+        if (editor.session.getUndoManager().isClean() && !nameChange) {
+            $('#save').prop("disabled",true);
             document.title = fileName;
             window.onbeforeunload = null;
         }
         else {
-            //console.log('no', editor.session.getUndoManager().isClean(), !nameChange);
-            //$('#save').removeClass("disabled");
+            $('#save').prop("disabled",false);
             document.title = "* " + fileName;
             window.onbeforeunload = function(){return "You have unsaved changes are you sure you want to exit?"};
         }
     }
 
-    //Load the editor
-    editor = ace.edit('editor');
-    editor.setTheme('ace/theme/monokai');
-    editor.getSession().setMode('ace/mode/' + lang);
+    editor.on('input', function() {
+        updateEditorState();
+    });
 
-    //Bind save keys
+    //Bin save keys
     editor.commands.addCommand({
         name: 'save',
         bindKey: {win: 'Ctrl-S', mac: 'Command-S'},
@@ -83,6 +110,7 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
     mousetrap.bind(['ctrl+s', 'command+s'], function(e){
         if (e.preventDefault) {
             e.preventDefault();
+            console.log("Prevented")
         } else {
             // internet explorer
             e.returnValue = false;
@@ -93,32 +121,22 @@ requirejs(['jquery', 'ace/ace', 'mousetrap'], function($, ace, mousetrap) {
         return false;
     });
 
-    //Update editor state when file is chnaged
-    editor.getSession().on('change', function(){updateEditorState()});
-
-    $(document).ready(function(){
-        //UI setup
-        var fileNameBox = $('#fileName');
-        if(fileType != '') {
-            $('#' + fileType).parent().addClass('active');
-            $('#fileNameContainer').find('label').html(fileType + '/')
+    var fileNameBox = $('#fileName');
+    if(fileType != '') {
+        $('#' + fileType).parent().addClass('active');
+        $('#fileNameContainer').find('label').html(fileType + '/')
+    }
+    if(fileName != '') {
+        fileNameBox.attr('value', fileName);
+    }
+    fileNameBox.on('input', function(e){
+        if($(e.target).val() != originalName) {
+            nameChange = true;
+            fileName = $(e.target).val();
+        } else {
+            nameChange = false;
+            fileName = originalName;
         }
-        if(fileName != '') {
-            fileNameBox.attr('value', fileName);
-        }
-        $('#save').on('click', function(){
-            save();
-        });
-        fileNameBox.on('input', function(e){
-            if($(e.target).val() != originalName) {
-                nameChange = true;
-                fileName = $(e.target).val();
-            } else {
-                nameChange = false;
-                fileName = originalName;
-            }
-            updateEditorState();
-        });
         updateEditorState();
     });
 });
